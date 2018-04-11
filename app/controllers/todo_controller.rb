dc755d20fb6aaa43e0f4ce8faec6215c179a7dbb
Grace Nwankwo ☼ <gnwankwo@cps.edu>
2018-04-11 13:56:42 +0000

    Install postgres and update database.yml v

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,13 @@ class TodoController < ApplicationController
     def index
     end
     def show
-    @todo_description = "Learn Variables"
+    todo_id = '1'
+  if todo_id == '1'
+    @todo_description = "Make the curriculum"
     @todo_pomodoro_estimate = 4
-    end
+  elsif todo_id == '2'
+    @todo_description = "Buy workshop supplies"
+    @todo_pomodoro_estimate = 3
+  end
+end
 end
